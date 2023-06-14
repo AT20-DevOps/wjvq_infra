84f1295798a1cf6e3c1f311ea29b1f8189b7d5d1
Williams Velasquez <Williams.Velasquez@jala.university>
2023-06-14 09:12:12 -0400

    Task13

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,17 +11,36 @@ resource "virtualbox_vm" "node" {
     count = 1
     name = format("node-%02d", count.index + 1)
     image = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
-    cpus = 1
-    memory = "512 mib"
+    cpus = 2
+    memory = "2056 mib"
     user_data = ""
     network_adapter {
-        type = "hostonly"
-        host_interface = "VirtualBox Host-Only Ethernet Adapter"
+        type = "bridged"
+        host_interface = "Realtek PCIe GbE Family Controller"
     }
+
+  connection {
+    type = "ssh"
+    host = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address,1)
+    user = "vagrant"
+    private_key = file("vagrant")
+  }
+  
+  provisioner "file" {
+    source = "../scripts/install_docker.sh"
+    destination = "/tmp/install_docker.sh"
+  }
+
+  //provision
+  provisioner "remote-exec" {
+    inline = [ 
+      "chmod +x /tmp/install_docker.sh",
+      "/tmp/install_docker.sh" 
+      ]
+    
+  }
 }
+
 output "IPAddr" {
-    value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 1)
-}
-output "IPAddr_2" {
-    value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 2)
+  value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 1)
 }
